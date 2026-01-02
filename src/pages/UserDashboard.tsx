528695b2e6534df48940c7b2004c4972fdf38c4d
haran2005 <hariharan.srk2005@gmail.com>
2026-01-02 11:48:55 +0530

    update:frontend

--- src/pages/UserDashboard.tsx
+++ src/pages/UserDashboard.tsx
@@ -1,23 +1,23 @@
-import Banner from '../components/layout/Banner'
-import Card from '../components/base/Card'
-const UserDashboard = () => {
-  return (
-    <>
-    <section className="w-full h-auto">
-    <Banner name='Haran' position='--manager'/>
-        <main className="w-full h-auto flex items-center justify-center gap-5 m-5">
-          <Card 
-            lable='Attendance'
-            value={10}
-            color='#0496ff'/>
-          <Card 
-            lable='Leaves'
-            value={10}
-            color='#0496ff'/>
-        </main>
-    </section>
-    </>
-  )
-}
+// import Banner from '../components/layout/Banner'
+// import Card from '../components/base/Card'
+// const UserDashboard = () => {
+//   return (
+//     <>
+//     <section className="w-full h-auto">
+//     <Banner name='Haran' position='--manager'/>
+//         <main className="w-full h-auto flex items-center justify-center gap-5 m-5">
+//           <Card 
+//             lable='Attendance'
+//             value={10}
+//             color='#0496ff'/>
+//           <Card 
+//             lable='Leaves'
+//             value={10}
+//             color='#0496ff'/>
+//         </main>
+//     </section>
+//     </>
+//   )
+// }
 
-export default UserDashboard
+// export default UserDashboard
